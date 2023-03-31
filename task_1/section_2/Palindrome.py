7668ef3765bcee5b285f5d9e0a5f864f1e642ae3
acarcastillo <carloscastilloaleman@gmail.com>
2023-03-30 18:59:27 -0600

    Ejercicio de palindromos

---
+++ task_1/section_2/Palindrome.py
@@ -0,0 +1,21 @@
+print ("Hello")
+
+def is_palindrome(input_user):
+    input_user = input_user.replace(" ", "").lower()
+    return input_user == input_user[::-1]
+
+
+def validate_input_user(): 
+    input_user = str(input("Ingrese un palindromo \n"))
+
+    if is_palindrome(input_user):
+        print(f" The phrase '{input_user}' is a palindrome")
+    else:
+        print("The phrase '{}' is not a palindrome".format(input_user))
+
+if __name__ == '__main__':
+    validate_input_user()        
+
+
+
+
